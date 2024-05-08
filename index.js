f2148dfda40b0a3db771cd0010dc83046a815479
tungbeoooo232 <thokroniumpun0suy@hotmail.com>
2024-05-09 06:24:47 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  return isMirror(root.left, root.right);
-  function isMirror(left, right) {
-    if (!left && !right) return true;
-    if (!left || !right || left.val !== right.val) return false;
-    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
-  }
-}
+const initial = (arr) => arr.slice(0, -1);
